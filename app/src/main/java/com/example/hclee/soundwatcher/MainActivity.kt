c504708b568bf09c467939dd9b5a8650c76e9b22
hclee <dlgockd45@gmail.com>
2019-04-23 16:08:34 +0900

    Add crawling sample code using Jsoup

--- app/src/main/java/com/example/hclee/soundwatcher/MainActivity.kt
+++ app/src/main/java/com/example/hclee/soundwatcher/MainActivity.kt
@@ -2,11 +2,40 @@ package com.example.hclee.soundwatcher
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.select.Elements
 
 class MainActivity : AppCompatActivity() {
+    private val TAG: String = MainActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        showTopHundred()
+    }
+
+    private fun showTopHundred() {
+        val url: String = "https://www.melon.com/chart/index.htm"
+        val cssQuery: String = "div.ellipsis>span>a"
+
+        tryJsoup(url, cssQuery)
+    }
+
+    private fun tryJsoup(url: String, cssQuery: String) {
+        var count: Int = 1
+
+        Thread(Runnable {
+            val document: Document = Jsoup.connect(url).get()
+            val elements: Elements = document.select(cssQuery)
+
+            for(element in elements) {
+                val song: String = element.text()
+
+                Log.d(TAG, "${count++}번째 $song")
+            }
+        }).start()
     }
 }
