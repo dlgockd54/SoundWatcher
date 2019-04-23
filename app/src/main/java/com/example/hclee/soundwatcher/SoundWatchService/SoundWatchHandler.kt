0c7adc298705816981fa834c3a796d91afd8adc9
hclee <dlgockd45@gmail.com>
2019-04-23 18:06:08 +0900

    Add SoundWatchHandler

---
+++ app/src/main/java/com/example/hclee/soundwatcher/SoundWatchService/SoundWatchHandler.kt
@@ -0,0 +1,46 @@
+package com.example.hclee.soundwatcher.SoundWatchService
+
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+import android.util.Log
+
+/**
+ * Created by hclee on 2019-04-23.
+ */
+
+class SoundWatchHandler(looper: Looper): Handler(looper) {
+    private val TAG: String = SoundWatchHandler::class.java.simpleName
+
+    init {
+        Log.d(TAG, "init")
+    }
+
+    companion object {
+        const val MSG_TOP_HUNDRED: Int = 222
+        const val MSG_NEW_ALBUM: Int = 333
+    }
+
+    override fun handleMessage(msg: Message?) {
+        Log.d(TAG, "handleMessage()")
+        Log.d(TAG, "what: ${msg?.what}")
+
+        var obj: Any
+
+        when(msg?.what) {
+            MSG_TOP_HUNDRED -> {
+                msg.obj.let {
+                    obj = it
+
+                    Log.d(TAG, "${obj as String} found!")
+                }
+            }
+            MSG_NEW_ALBUM -> {
+
+            }
+            else -> {
+
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/hclee/soundwatcher/SoundWatchService/TopHundredPullingThread.kt
+++ app/src/main/java/com/example/hclee/soundwatcher/SoundWatchService/TopHundredPullingThread.kt
@@ -1,5 +1,7 @@
 package com.example.hclee.soundwatcher.SoundWatchService
 
+import android.os.Looper
+import android.os.Message
 import android.util.Log
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
@@ -9,14 +11,22 @@ import org.jsoup.select.Elements
  * Created by hclee on 2019-04-23.
  */
 
+/**
+ * Pull top hundred chart from Melon.
+ * Use main looper for handle message.
+ */
 class TopHundredPullingThread: Thread() {
     private val TAG: String = TopHundredPullingThread::class.java.simpleName
     private val CHART_URL: String = "https://www.melon.com/chart/index.htm"
     private val CHART_QUERY: String = "div.ellipsis>span>a"
     private var count: Int = 1
 
+    private lateinit var mHandler: SoundWatchHandler
+
     override fun run() {
         Log.d(TAG, "run()")
+
+        mHandler = SoundWatchHandler(Looper.getMainLooper())
 
         showTopHundred()
     }
@@ -29,8 +39,20 @@ class TopHundredPullingThread: Thread() {
 
         for(element in elements) {
             val song: String = element.text()
+            val isContainsString: Boolean = song.contains("장범준")
 
             Log.d(TAG, "${count++}번째 $song")
+
+            if(isContainsString) {
+                Log.d(TAG, "contains!")
+
+                val message: Message = mHandler.obtainMessage(SoundWatchHandler.MSG_TOP_HUNDRED, "장범준")
+
+                mHandler.sendMessage(message)
+            }
+            else {
+                Log.d(TAG, "not contains!")
+            }
         }
     }
 }
